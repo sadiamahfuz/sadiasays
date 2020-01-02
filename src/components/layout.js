7693f93e1c8a2350d6feb68fa67e77da4e2f33e1
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2020-01-02 08:53:39 +0000

    skeleton pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle="Sadia Says" />
       <div
         style={{
           margin: `0 auto`,
@@ -36,9 +36,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
