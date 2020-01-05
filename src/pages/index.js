195d3bdde90e7cdbf8a8ad77771b903efeb7bcc5
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2020-01-05 20:50:55 +0000

    add images

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <img src={logo} alt="Logo" />
+    </div>
+  )
+};
