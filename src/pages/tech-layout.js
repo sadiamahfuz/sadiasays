e1f30192da96b7ee22a5aba0834cf9fa4030bef6
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2020-05-23 18:13:52 +0100

    import rating component

--- src/pages/tech-layout.js
+++ src/pages/tech-layout.js
@@ -3,28 +3,32 @@ import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/Layout"
 import styled from "styled-components"
+import { Rating } from '@material-ui/lab';
+import { withStyles } from '@material-ui/core/styles';
 
 const Title = styled.h3`
   color: #9d036a;
   margin-bottom: 0;
   text-transform: capitalize;
-`
+`;
 
 const Date = styled.p`
   margin-top: 0;
   color: #9d929b;
   font-size: 0.9rem;
-`
-
-const Rating = styled.p`
-  color: #9d036a;
-`
+`;
 
 const Image = styled.img`
   display: block;
   margin-left: auto;
   margin-right: auto;
-`
+`;
+
+const StyledRating = withStyles({
+    iconFilled: {
+        color: '#9d036a'
+    }
+})(Rating);
 
 export default ({ data: { mdx } }) => {
   const image = require("../../content/" + mdx.frontmatter.thumbnail.relativePath);
@@ -32,7 +36,7 @@ export default ({ data: { mdx } }) => {
     <Layout>
       <Title>{mdx.frontmatter.title}</Title>
       <Date>{mdx.frontmatter.date}</Date>
-      {mdx.frontmatter.rating ? <Rating>RATING: {mdx.frontmatter.rating} / 5</Rating> : null}
+      {mdx.frontmatter.rating ? <StyledRating defaultValue={mdx.frontmatter.rating} readOnly={true}/> : null}
       <Image src={image} alt="image" />
       <MDXRenderer>{mdx.body}</MDXRenderer>
 
